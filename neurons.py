2636ecbe85f343dcc712559f52905541b4bc74fa
Matthew Athanasopoulos <matthew.athanasopoulos@gmail.com>
2023-03-04 21:06:01 -0500

    Added second layer and numpy approaches

--- neurons.py
+++ neurons.py
@@ -1,11 +1,30 @@
-inputs = [1, 2, 3, 2.5]
+import numpy as np
 
-weights = [[0.2, 0.8, -0.5, 1.0], [0.5, -0.91,
-                                   0.26, -0.5], [-0.26, -0.27, 0.17, 0.87]]
-biases = [2, 3, 0, 5]
+inputs = [[1, 2, 3, 2.5],
+          [2.0, 5.0, -1.0, 2.0],
+          [-1.5, 2.7, 3.3, -0.8]]
 
+weights = [[0.2, 0.8, -0.5, 1.0],
+           [0.5, -0.91, 0.26, -0.5],
+           [-0.26, -0.27, 0.17, 0.87]]
+
+biases = [2, 3, 0.5]
+
+weights2 = [[0.1, -0.14, 0.5],
+            [-0.5, -0.12, -0.33],
+            [-0.44, 0.73, -0.13]]
+
+biases2 = [-1, 2, -0.5]
+
+layer1_outputs = np.dot(inputs, np.array(weights).T) + biases
+
+layer2_outputs = np.dot(layer1_outputs, np.array(weights2).T) + biases2
+
+print(layer2_outputs)
+
+
+'''
 layer_outputs = []  # Output of current layer
-
 for neuron_weights, neuron_bias in zip(weights, biases):
     neuron_output = 0  # Output of given neuron
     for n_input, weight in zip(inputs, neuron_weights):
@@ -14,3 +33,4 @@ for neuron_weights, neuron_bias in zip(weights, biases):
     layer_outputs.append(neuron_output)
 
 print(layer_outputs)
+'''
