6eb689eaf32c2a3bd71244d533997ec993ef93f0
Matthew Athanasopoulos <matthew.athanasopoulos@gmail.com>
2023-03-04 21:48:11 -0500

    Create a dense layer and objects

--- neurons.py
+++ neurons.py
@@ -1,9 +1,32 @@
 import numpy as np
 
-inputs = [[1, 2, 3, 2.5],
-          [2.0, 5.0, -1.0, 2.0],
-          [-1.5, 2.7, 3.3, -0.8]]
+np.random.seed(0)
 
+X = [[1, 2, 3, 2.5],
+     [2.0, 5.0, -1.0, 2.0],
+     [-1.5, 2.7, 3.3, -0.8]]
+
+
+class Layer_Dense:
+    def __init__(self, n_inputs, n_neurons):
+        self.weights = 0.10 * np.random.randn(n_inputs, n_neurons)
+        self.biases = np.zeros((1, n_neurons))
+
+    def forward(self, inputs):
+        self.output = np.dot(inputs, self.weights) + self.biases
+
+
+layer1 = Layer_Dense(4, 5)
+layer2 = Layer_Dense(5, 2)
+layer1.forward(X)
+
+# print(layer1.output)
+
+layer2.forward(layer1.output)
+print(layer2.output)
+
+
+'''
 weights = [[0.2, 0.8, -0.5, 1.0],
            [0.5, -0.91, 0.26, -0.5],
            [-0.26, -0.27, 0.17, 0.87]]
@@ -21,9 +44,9 @@ layer1_outputs = np.dot(inputs, np.array(weights).T) + biases
 layer2_outputs = np.dot(layer1_outputs, np.array(weights2).T) + biases2
 
 print(layer2_outputs)
+ 
+------
 
-
-'''
 layer_outputs = []  # Output of current layer
 for neuron_weights, neuron_bias in zip(weights, biases):
     neuron_output = 0  # Output of given neuron
